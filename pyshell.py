ca2276add3847fdfc5498d5fb83297ac6c722909
Milan Jain <jameschabbra22@gmail.com>
2021-09-25 03:02:46 +0530

    making resposive for smaller screen

--- pyshell.py
+++ pyshell.py
@@ -1,69 +1,58 @@
 """
-Complete this program to implement the rudimentary
-interactive command shell. Implement the run()
-function below and make necessary changes to
-ensure that the shell works as per the transcript
-shown below:
-----------------------------------------------------
-    $ python pyshell.py
-    PyShell> list /bin /usr /opt
-    Listing contents of ("/bin", "/usr", "/opt")
+Implement the Fraction class below passing all
+the test cases in the doc-tests.
 
-    PyShell> date
-    Tue Aug 01 09:30:00 2017
-
-    PyShell> dslfjsdfdsf
-    Invalid command - dslfjsdfdsf
-
-    PyShell> exit
-    $
-------------------------------------------------------
-Implement a scalable solution; i.e., adding new commands
-must be mostly effortless and the shell command evaluation
-must be performance efficient.
-
-[ Download stub code from https://tinyurl.com/pyshell-ex ]
-
+    >>> a = Fraction(1, 2)
+    >>> b = Fraction("1/4")
+    >>> print(a)
+    1/2
+    >>> print(b)
+    1/4
+    >>> print(a + b)
+    3/4
+    >>> print(a - b)
+    1/4
+    >>> print(a * b)
+    1/8
+    >>> print(a / b)
+    2/1
+    >>> float(a)
+    0.5
+    >>> float(b)
+    0.25
+    >>> c = Fraction(33, 99)
+    >>> print(c)
+    1/3
+    
 """
+class Fraction:
+    """
+    This class implements fractional numbers.
+    If instantiated with two arguments in the constructor
+    expression, each argument must be an integer representing
+    the numerator and the denominator. If the constructor
+    expession is passed with single argument, then the argument
+    must be a string of the form "n/d" when n represents numerator
+    and d represents denominator.
 
-# Invoke this function if the command is 'list'
-def list_directory(*args):
-    from os import listdir
-    if len(args) < 2:
-        args += ".",
-    for path in args[1:]:
-        print("{}:".format(path))
-        print("\n".join(listdir(path)))
-
-# Invoke this function if the command is 'whoami'
-def show_user(*args):
-    from getpass import getuser
-    print(getuser())
-
-# Invoke this function if the command is 'date'
-def print_date(*args):
-    from time import ctime
-    print(ctime())
-
-# Invoke this function if the command is 'exit'
-def exit_shell(*args):
-    exit(0)
-
-# Invoke this function if the command is anything else
-def invalid_command(*args):
-    print("Invalid command - ", args[0])
+    For example:  
+        >>> a = Fraction(1, 2) # Create a fraction 1/2
+        >>> b = Fraction("1/4") # Create a fraction 1/4
+        >>> c = a + b   # Add the two fractions
+        >>> print(c)    # Print the string representation of c
+        3/4
+        >>> a > b
+        True
+        >>> c == b
+        False
 
 
-def run():
-    commands = {"list": list_directory, "date": print_date, "exit": exit_shell, "users": show_user}
-    while True:
-        command = input("PyShell> ")
-        command = command.split(" ")
-        if command[0] in commands:
-            commands[command[0]](command)
-        else :
-            invalid_command(command)
-
+    """
+    def __init__(self,num1,num2):
+        
+        self.num1 = num1
+        self.num2 = num2
 
 if __name__ == '__main__':
-    run()
+    import doctest
+    doctest.testmod()
